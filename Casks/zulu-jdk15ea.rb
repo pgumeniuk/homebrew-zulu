f081324856709b1705322b11f9da96cfa53bb821
Pavlo Gumeniuk <50366880+pgumeniuk@users.noreply.github.com>
2020-07-23 12:25:22 +0300

    Update zulu-jdk15ea.rb

--- Casks/zulu-jdk15ea.rb
+++ Casks/zulu-jdk15ea.rb
@@ -1,6 +1,6 @@
 cask 'zulu-jdk15ea' do
-  version '15.0.43,15.0.0-ea.19'
-  sha256 '0ab50623e55706e8567c0d6ed52bd795b4a1bc889dfc97be74ff361e5da6f4a7'
+  version '15.0.67,15.0.0-ea.31'
+  sha256 'd8d8872ebd8df06626dfa435a21f1d5352fdb725ff6d7b02df9a5f1a4c613524'
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.before_comma}-ea-jdk#{version.after_comma}-macosx_x64.tar.gz",
       referer: 'https://www.azul.com/downloads/zulu-community/'
