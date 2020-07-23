c0217b7d739f54484e452b28f52a2dad684d677f
Pavlo Gumeniuk <50366880+pgumeniuk@users.noreply.github.com>
2020-07-23 11:06:11 +0300

    Update zulu-jdk8.rb

--- Casks/zulu-jdk8.rb
+++ Casks/zulu-jdk8.rb
@@ -1,6 +1,6 @@
 cask 'zulu-jdk8' do
-    version '8.0.252,8.46.0.19'
-    sha256 '1fa97725adf5df15a84d835156d4ca35433af0d6b0a1aa724336152d4325b827'
+    version '8.0.262,8.48.0.51'
+    sha256 '7c4af8c22a11db6a83ebd0df8050449616d0aa48722f7596b60039803b86924a'
 
     url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-ca-jdk#{version.before_comma}-macosx_x64.dmg",
         referer: 'https://www.azul.com/downloads/zulu-community/'
